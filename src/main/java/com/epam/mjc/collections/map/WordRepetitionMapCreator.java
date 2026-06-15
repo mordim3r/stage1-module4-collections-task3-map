55cd083687cca52fb1e3aea9e29ef73836125bc3
mordim3r <valent1n.bazylev@gmai.com>
2026-06-15 14:05:31 +0300

    фвв

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -7,7 +7,7 @@ public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
 
         Map<String, Integer> result = new HashMap<>();
-        String []words = sentence.split(" ");
+        String []words = sentence.split("[^a-zA-Z]+");
         for (String i: words){
             String lword = i.toLowerCase();
             result.merge(lword,1, (a,b)->a+b);
